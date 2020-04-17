8928b275296751c8f5b5cf39f4736e7e5a7dae50
cocowalla <800977+cocowalla@users.noreply.github.com>
2020-04-17 21:45:13 +0100

    Rename field to alwaysWriteHeader

--- src/Serilog.Sinks.File.Header/HeaderWriter.cs
+++ src/Serilog.Sinks.File.Header/HeaderWriter.cs
@@ -14,27 +14,39 @@ namespace Serilog.Sinks.File.Header
         // Same as the default StreamWriter buffer size
         private const int DEFAULT_BUFFER_SIZE = 1014;
 
+        // Factory method to generate the file header
         private readonly Func<string> headerFactory;
 
-		public bool SkipStreamLengthCheck { get; set; }
+        // Whether to always write the header, even for non-empty files
+		private readonly bool alwaysWriteHeader;
 
-        public HeaderWriter(string header)
+        public HeaderWriter(string header, bool alwaysWriteHeader = false)
         {
             this.headerFactory = () => header;
+            this.alwaysWriteHeader = alwaysWriteHeader;
         }
 
-		public HeaderWriter(Func<string> headerFactory)
+		public HeaderWriter(Func<string> headerFactory, bool alwaysWriteHeader = false)
         {
             this.headerFactory = headerFactory;
+            this.alwaysWriteHeader = alwaysWriteHeader;
         }
 
         public override Stream OnFileOpened(Stream underlyingStream, Encoding encoding)
         {
-			if (!this.SkipStreamLengthCheck && underlyingStream.Length != 0)
-			{
-				SelfLog.WriteLine($"File header will not be written, as the stream already contains {underlyingStream.Length} bytes of content");
-                return base.OnFileOpened(underlyingStream, encoding);
-			}
+            try
+            {
+			    if (!this.alwaysWriteHeader && underlyingStream.Length != 0)
+			    {
+				    SelfLog.WriteLine($"File header will not be written, as the stream already contains {underlyingStream.Length} bytes of content");
+                    return base.OnFileOpened(underlyingStream, encoding);
+			    }
+            }
+            catch (NotSupportedException)
+            {
+                // Not all streams support reading the length - in this case, we always write the header, 
+                // otherwise we'd *never* write it!
+            }
 
 			using (var writer = new StreamWriter(underlyingStream, encoding, DEFAULT_BUFFER_SIZE, true))
 			{
